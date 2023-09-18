bd0fec773321c9c0ac1f4696c99c90cd7a9ea5cf
Nahorniy Roman <nahornyi.r22@fpm.dnu.edu.ua>
2023-09-18 14:26:41 +0300

    Add project files.

---
+++ Lab1/Person.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Person : IDateAndCopy, IComparable, IComparer<Person>
+    {
+        protected string name;
+        protected string surname;
+        protected DateTime dateOfBirth;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+        public DateTime Date
+        {
+            get
+            {
+                return dateOfBirth;
+            }
+            set
+            {
+                dateOfBirth = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return dateOfBirth.Year;
+            }
+            set
+            {
+                dateOfBirth = new DateTime(value, dateOfBirth.Month, dateOfBirth.Day);
+            }
+        }
+        public Person()
+        {
+            Name = "Roman";
+            Surname = "Nagorniy";
+            Date = DateTime.Parse("08.07.2005");
+        }
+        public Person(string name, string surname, DateTime date)
+        {
+            Name = name;
+            Surname = surname;
+            Date = date;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Surname: {Surname}, DateOfBirth: {Date}";
+        }
+        public virtual string ToShortString()
+        {
+            return $"Name: {Name}, Surname: {Surname}";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Person && obj != null)
+            {
+                Person tmp;
+                tmp = (Person)obj;
+                if (tmp.Name == this.Name && tmp.Surname == this.Surname && tmp.Date == Date) {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            return false;
+        }
+        public static bool operator ==(Person a, Person b)
+        { return a.Equals(b); }
+        public static bool operator !=(Person a, Person b)
+        { return !a.Equals(b); }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public virtual object DeepCopy()
+        {
+            return new Person(Name, Surname, Date);
+        }
+        int IComparable.CompareTo(object? obj)
+        {
+            Person tmp = obj as Person;
+            if (tmp == null)
+            {
+                throw new ArgumentException("Wrong");
+            }
+            else
+            {
+                return string.Compare(Surname, tmp.Surname);
+            }
+        }
+        int IComparer<Person>.Compare(Person p1, Person p2)
+        {
+            return p1.Date.CompareTo(p2.Date);
+        }
+        
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,50 @@
+﻿using Lab3;
+using System;
+using System.Reflection;
+//1.Створити об'єкт типу EmployeeCollection. Додати до колекції кілька різних елементів типу Employee і вивести об'єкт EmployeeCollection.
+EmployeeCollection employeeCollection = new EmployeeCollection(new Employee(new Person("Ivan", "B", DateTime.Parse("03.03.2005")), "0", TimeWork.FullTime, 200));
+employeeCollection.AddEmployees(new Employee(new Person("Petro", "C", DateTime.Parse("01.01.2005")), "0", TimeWork.FullTime, 100), new Employee(new Person("Roman", "A", DateTime.Parse("02.02.2005")), "0", TimeWork.FullTime, 300));
+Console.WriteLine(employeeCollection);
+Console.WriteLine("=======================================");
+//2.Для створеного об'єкта EmployeeCollection викликати методи, які виконують сортування списку List<Employee> за різними критеріями, і після кожного сортування вивести дані об'єкта. Виконати сортування
+//за  прізвищем співробітника
+employeeCollection.SortBySurname();
+Console.WriteLine(employeeCollection);
+Console.WriteLine("=======================================");
+//за датою народження
+employeeCollection.SortByDate();
+Console.WriteLine(employeeCollection);
+Console.WriteLine("=======================================");
+//за окладом
+employeeCollection.SortBySalary();
+Console.WriteLine(employeeCollection);
+Console.WriteLine("=======================================");
+//3.Створити об'єкт типу TestCollections. Викликати метод для пошуку в колекціях першого, центрального, останнього і елемента, що не входить в колекції. Вивести  значення  часу  пошуку  для  всіх  чотирьох  випадків  з поясненнями  про  те,  для  якої  колекції  і  для  якого  елементу  отримано значення.
+int number = 10000;
+TestCollections testCollections = new TestCollections(number);
+string middleIndex = (number / 2).ToString();
+string lastIndex = (number - 1).ToString();
+string[] indices = { "0", middleIndex, lastIndex, number.ToString() };
+
+foreach(string index in indices)
+{
+    Person testPerson = new Person(index, index, new DateTime());
+    Employee testEmployee = new Employee(testPerson, "Beginner", TimeWork.Free, 100);
+    Console.WriteLine("Index: " + index);
+    Console.WriteLine("=======================================");
+    Console.WriteLine("Search Person: " + testCollections.SearchPerson(testPerson).ToString());
+    Console.WriteLine();
+    Console.WriteLine("Search String: " + testCollections.SearchString(testEmployee.ToString()).ToString());
+    Console.WriteLine();
+    Console.WriteLine("SearchByKeyInDictionary1: " + testCollections.SearchByKeyInDictionary1(testPerson).ToString());
+    Console.WriteLine();
+    Console.WriteLine("SearchByKeyInDictionary2: " + testCollections.SearchByKeyInDictionary2(testEmployee.ToString()).ToString());
+    Console.WriteLine();
+    Console.WriteLine("SearchByValueInDictionary1: " + testCollections.SearchByValueInDictionary1(testEmployee).ToString());
+    Console.WriteLine();
+    Console.WriteLine("SearchByValueInDictionary2: " + testCollections.SearchByValueInDictionary2(testEmployee).ToString());
+    Console.WriteLine();
+    Console.WriteLine("=======================================");
+}
+
+
